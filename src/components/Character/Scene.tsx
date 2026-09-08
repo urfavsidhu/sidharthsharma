03baf4e79b1c5fcf66b89beb56f8c8dd4f03b8b5
Sidharth Sharma <sidharth14949@gmail.com>
2026-09-08 12:03:26 +0530

    Update Scene.tsx

--- src/components/Character/Scene.tsx
+++ src/components/Character/Scene.tsx
@@ -32,7 +32,7 @@ const Scene = () => {
         antialias: true,
       });
       renderer.setSize(container.width, container.height);
-      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
       renderer.toneMapping = THREE.ACESFilmicToneMapping;
       renderer.toneMappingExposure = 1;
       canvasDiv.current.appendChild(renderer.domElement);
