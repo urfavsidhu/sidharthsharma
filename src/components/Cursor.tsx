87f91e66e65835732015e38b8da2a1496a94764c
Sidharth Sharma <sidharth14949@gmail.com>
2026-09-08 12:02:27 +0530

    Update Cursor.tsx

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -18,8 +18,7 @@ const Cursor = () => {
         const delay = 6;
         cursorPos.x += (mousePos.x - cursorPos.x) / delay;
         cursorPos.y += (mousePos.y - cursorPos.y) / delay;
-        gsap.to(cursor, { x: cursorPos.x, y: cursorPos.y, duration: 0.1 });
-        // cursor.style.transform = `translate(${cursorPos.x}px, ${cursorPos.y}px)`;
+        cursor.style.transform = `translate(${cursorPos.x}px, ${cursorPos.y}px)`;
       }
       requestAnimationFrame(loop);
     });
